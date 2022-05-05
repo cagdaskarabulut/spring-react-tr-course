ad022b241348ed88c0d7f47b6b1b2e5757ac7fd1
Cagdas Karabulut <ckarabulut@adersystems.com>
2022-05-05 14:03:42 +0300

    .

---
+++ ws/src/main/resources/static/precache-manifest.abaf099351e09e44bd7ac6964fb1b979.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e1c70b9c826c936bac97ea51a2f2fb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5025812bfe3fff01b14d",
+    "url": "/static/css/main.189e8dfd.chunk.css"
+  },
+  {
+    "revision": "4a8b20df60af978e475b",
+    "url": "/static/js/2.e042978d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.e042978d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5025812bfe3fff01b14d",
+    "url": "/static/js/main.3eea1f94.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  },
+  {
+    "revision": "2dcb4821b18b3e6060236b90b13c136b",
+    "url": "/static/media/hoaxify.2dcb4821.png"
+  },
+  {
+    "revision": "926fda932fcddf9dae79b8a3a560745b",
+    "url": "/static/media/profile.926fda93.png"
+  }
+]);
